dfe6ccc8c22dec7b566f2e503175294881581cd3
IraSkiba <macbook@Macbooks-MacBook-Pro.local>
2023-11-08 17:50:29 +0300

    added fetch to form and contentbox

--- src/components/Contentbox/Contentbox.js
+++ src/components/Contentbox/Contentbox.js
@@ -1,18 +1,32 @@
 import React from "react";
 import './Contentbox.css';
 import {Card} from "../Card";
-import { data } from '../Mock/Mock'
 
 
-export const Contentbox = (item) => (
-  <div className="section-homes__items">
-    {data.slice(0,4).map((img) => (
-      <Card key={img.id}
-        name={img.name}
-            city={img.city}
-            country={img.country}
-            image={img.imageUrl}/>
-    )) }
+export class Contentbox extends React.Component{
+constructor(props) {
+  super(props);
+  this.state ={
+    hotels :[]
+  }
+}
+  componentDidMount() {
+    const URL = 'https://if-student-api.onrender.com/api/hotels/popular';
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => this.setState({hotels:data}))
+  }
+  render(){
+    return <div className="section-homes__items">
+      {this.state.hotels.slice(0,4).map((img) => (
+        <Card key={img.id}
+              name={img.name}
+              city={img.city}
+              country={img.country}
+              image={img.imageUrl}/>
+      )) }
 
-  </div>
-);
+    </div>
+
+  }
+}
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,54 +2,83 @@
 import './Form.css'
 import {Input} from "../Input";
 import {Btn} from "../Btn";
-import {data} from '../Mock';
-import {useState} from "react";
 import {filtred} from '../Mock/filterH'
+import React, {Component} from "react";
 
-export const Form = ({setCity}) => {
-  const [inputCity, setInputCity] = useState('')
-  const handleChange = (event) => {
+
+export class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputCity: '',
+    }
+  }
+  URL = new URL('https://if-student-api.onrender.com/api/hotels')
+  param = new URLSearchParams();
+
+  getAll() {
+    this.URL.search = this.param.toString();
+    return this.URL.toString();
+  }
+
+  handleChange = (event) => {
+
     event.preventDefault()
-    if(event.target.name ==='city'){
-      setInputCity(event.target.value);
+    if (event.target.name === 'city') {
+      this.setState({inputCity: event.target.value});
+      this.param.set('search', event.target.value.toString());
+     this.URL.search = this.param.toString();
     }
   };
 
-  const handleClick = (event) => {
+  handleClick = (event) => {
     event.preventDefault()
-    let filter = filtred(data, inputCity);
-    setCity(filter)
-    setInputCity('')
+    const URL = 'https://if-student-api.onrender.com/api/hotels'
+    fetch(this.URL)
+      .then((response) => response.json())
+      .then((data) => {
+        let filter = filtred(data, this.state.inputCity);
+        this.props.setCity(filter)
+        this.URL.search = this.param.toString();
+        const fullUrl = this.getAll();
+        console.log(fullUrl)
+        //this.setState({inputCity: ''});
+      })
   }
-   return(
-   <div className="form-section" >
-     <form onSubmit={handleClick} >
-       <Input onChange={handleChange} htmlFor="city" value={inputCity} name="city" id="city"/>
-       <div className="form-section__date-element">
-         <label className="form-section__date-element--date__label" htmlFor="date">Check-in — Check-out</label>
-         <input className="form-section__date-element--date__form"
-                type="text"
-                id="date"
-                defaultValue="Tue 15 Sept - Sat 19 Sept"/>
-       </div>
-       <div id="counter-block" className="counter-block">
-         <div className="counter-block counter-block__adults">
-           <span className="span_number" id="adult-span"> 0 </span>
-           <label className="_label" htmlFor="adult-span">Adults —</label>
-         </div>
 
-         <div className="counter-block counter-block__children">
-           <span className="span_number" id="children-span"> 0 </span>
-           <label className="_label" htmlFor="children-span">Children —</label>
-         </div>
-         <div className="counter-block counter-block__rooms">
-           <span className="span_number" id="rooms-span"> 0 </span>
-           <label className="_label" htmlFor="rooms-span">Room</label>
-         </div>
-       </div>
-<Btn/>
-     </form>
-   </div>
-   )
- };
+
+  render() {
+    return (
+      <div className="form-section">
+        <form onSubmit={this.handleClick}>
+          <Input onChange={this.handleChange} htmlFor="city" value={this.state.inputCity} name="city" id="city"/>
+          <div className="form-section__date-element">
+            <label className="form-section__date-element--date__label" htmlFor="date">Check-in — Check-out</label>
+            <input className="form-section__date-element--date__form"
+                   type="text"
+                   id="date"
+                   defaultValue="Tue 15 Sept - Sat 19 Sept"/>
+          </div>
+          <div id="counter-block" className="counter-block">
+            <div className="counter-block counter-block__adults">
+              <span className="span_number" id="adult-span"> 0 </span>
+              <label className="_label" htmlFor="adult-span">Adults —</label>
+            </div>
+
+            <div className="counter-block counter-block__children">
+              <span className="span_number" id="children-span"> 0 </span>
+              <label className="_label" htmlFor="children-span">Children —</label>
+            </div>
+            <div className="counter-block counter-block__rooms">
+              <span className="span_number" id="rooms-span"> 0 </span>
+              <label className="_label" htmlFor="rooms-span">Room</label>
+            </div>
+          </div>
+          <Btn/>
+        </form>
+      </div>
+    )
+
+  }
+}
 
